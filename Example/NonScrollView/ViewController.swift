3424b4d97d9e99adcb9e9db2b721570c8cd77c1f
Shangxin Guo <int123c@gmail.com>
2018-11-10 21:48:07 +0800

    Initial commit

---
+++ Example/NonScrollView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NonScrollView
+//
+//  Created by int123c on 11/10/2018.
+//  Copyright (c) 2018 int123c. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
